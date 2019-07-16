1d832be6bd9930c0cde2b0e6306cb3e3c77a1587
Emily Becher <358274@bsd48.org>
2019-07-16 14:29:55 -0700

    Style changes to GUI

--- HandwritingGUI.py
+++ HandwritingGUI.py
@@ -6,9 +6,9 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 import subprocess
 import numpy as np
-import cv2
 import PIL
 
+#Runs programs when buttons are clicked
 def run_comparison():
     print ("Running Comparison")
     #subprocess.call(["python", "comparison program"])
@@ -19,6 +19,7 @@ def run_reader():
     print ("Running Reader")
     #subprocess.call(["python", "reader program"])
 
+#Creates Window
 window = tk.Tk()
 window.title("Welcome to the Handwriting Classifier")
 window.geometry('459x300')
@@ -26,6 +27,7 @@ window.geometry('459x300')
 path1 = "1.jpg"
 path2 = "2.jpg"
 
+#Creates Tabs
 tabControl = ttk.Notebook(window)
 tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text='Programs')
@@ -47,25 +49,28 @@ btn3 = tk.Button(tab1, text=("Run Reader"), padx=25, pady=5, command=run_reader)
 btn3.grid(column=2, row=1)
 
 #Tab 2
+title = tk.Label(tab2, text="Results & Images", font=("Arial Bold", 20), padx=110, pady=5)
+title.grid(columnspan=3, row=0)
+
 #Image 1
-img1 = Image.open('photos/1.jpg') # image extension *.png,*.jpg 
+img1 = Image.open('photos/1.jpg')
 img1 = img1.resize((200, 200), PIL.Image.ANTIALIAS)
 img1.save('1.jpg')
 img1 = ImageTk.PhotoImage(Image.open(path1))
 
 panel1 = tk.Label(tab2, image = img1)
-panel1.grid(column=0, row=0)
+panel1.grid(column=0, row=1)
 
 #Image 2
-img2 = Image.open('photos/2.jpg') # image extension *.png,*.jpg 
+img2 = Image.open('photos/2.jpg')
 img2 = img2.resize((200, 200), PIL.Image.ANTIALIAS)
 img2.save('2.jpg')
 img2 = ImageTk.PhotoImage(Image.open(path2))
 
 panel2 = tk.Label(tab2, image = img2)
-panel2.grid(column=1, row=0)
+panel2.grid(column=2, row=1)
 
-window.mainloop()
+#window.mainloop()
 
 
 
