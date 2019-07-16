9dac56c1ab0613538f8d92b06a09240a37d5cd81
Emily Becher <358274@bsd48.org>
2019-07-16 11:05:49 -0700

    new changes to gui file

--- HandwritingGUI.py
+++ HandwritingGUI.py
@@ -1,36 +1,47 @@
 #Handwriting Classifier GUI
 #7/15/2019
 
-from tkinter import*
+import tkinter as tk
+from tkinter import ttk
 
 import subprocess
 
 def run_comparison():
     print ("Success")
-    subprocess.call(["python", "ButtonTest.py"])
+    subprocess.call(["python", "comparison.py"])
 
-window = Tk()
+window = tk.Tk()
 
 window.title("Welcome to the Handwriting Classifier")
 
 window.geometry('459x300')
 
-lbl = Label(window, text="Handwriting", font=("Arial Bold", 20), padx=25, pady=5)
+tabControl = ttk.Notebook(window)
+tab1 = ttk.Frame(tabControl)
+tabControl.add(tab1, text='Programs')
+tabControl.pack(expand=1, fill="both")
+tab2 = ttk.Frame(tabControl)
+tabControl.add(tab2, text="Results")
+
+lbl = tk.Label(tab1, text="Handwriting", font=("Arial Bold", 20), padx=25, pady=5)
 
 lbl.grid(column=1, row=0)
 
-btn1 = Button(window, text="Run Comparison", padx=14, pady=5, command=run_comparison)
+btn1 = tk.Button(tab1, text="Run Comparison", padx=14, pady=5, command=run_comparison)
 
 btn1.grid(column=0, row=1)
 
-btn2 = Button(window, text=("Run Profiler"), padx=25, pady=5)
+btn2 = tk.Button(tab1, text=("Run Profiler"), padx=25, pady=5)
 
 btn2.grid(column=1, row=1)
 
-btn3 = Button(window, text=("Run Reader"), padx=25, pady=5)
+btn3 = tk.Button(tab1, text=("Run Reader"), padx=25, pady=5)
 
 btn3.grid(column=2, row=1)
 
 window.mainloop()
+
+
+
 
 
