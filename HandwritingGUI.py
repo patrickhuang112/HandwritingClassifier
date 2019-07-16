37f6f47809f90292c68d624a0371c57a2bcfa305
Emily Becher <358274@bsd48.org>
2019-07-16 13:31:51 -0700

    Working Images in GUI

--- HandwritingGUI.py
+++ HandwritingGUI.py
@@ -7,19 +7,24 @@ from PIL import ImageTk, Image
 import subprocess
 import numpy as np
 import cv2
-
-# Load an color image in grayscale
-img = cv2.imread('1.jpg',0)
+import PIL
 
 def run_comparison():
-    print ("Success")
-    subprocess.call(["python", "comparison.py"])
+    print ("Running Comparison")
+    #subprocess.call(["python", "comparison program"])
+def run_profiler():
+    print ("Running Profiler")
+    #subprocess.call(["python", "profiler program"])
+def run_reader():
+    print ("Running Reader")
+    #subprocess.call(["python", "reader program"])
 
 window = tk.Tk()
 window.title("Welcome to the Handwriting Classifier")
 window.geometry('459x300')
 
-path = "photos/1.jpg"
+path1 = "1.jpg"
+path2 = "2.jpg"
 
 tabControl = ttk.Notebook(window)
 tab1 = ttk.Frame(tabControl)
@@ -35,21 +40,30 @@ lbl.grid(column=1, row=0)
 btn1 = tk.Button(tab1, text="Run Comparison", padx=14, pady=5, command=run_comparison)
 btn1.grid(column=0, row=1)
 
-btn2 = tk.Button(tab1, text=("Run Profiler"), padx=25, pady=5)
+btn2 = tk.Button(tab1, text=("Run Profiler"), padx=25, pady=5, command=run_profiler)
 btn2.grid(column=1, row=1)
 
-btn3 = tk.Button(tab1, text=("Run Reader"), padx=25, pady=5)
+btn3 = tk.Button(tab1, text=("Run Reader"), padx=25, pady=5, command=run_reader)
 btn3.grid(column=2, row=1)
 
 #Tab 2
-img = Image.open('photos/1.jpg') # image extension *.png,*.jpg 
-img = img.resize((200, 200), Image.ANTIALIAS)
-img.save('1.jpg')
-#img = img.resize((200, 200), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(Image.open(path))
+#Image 1
+img1 = Image.open('photos/1.jpg') # image extension *.png,*.jpg 
+img1 = img1.resize((200, 200), PIL.Image.ANTIALIAS)
+img1.save('1.jpg')
+img1 = ImageTk.PhotoImage(Image.open(path1))
 
-panel = tk.Label(tab2, image = img)
-panel.grid(column=0, row=0)
+panel1 = tk.Label(tab2, image = img1)
+panel1.grid(column=0, row=0)
+
+#Image 2
+img2 = Image.open('photos/2.jpg') # image extension *.png,*.jpg 
+img2 = img2.resize((200, 200), PIL.Image.ANTIALIAS)
+img2.save('2.jpg')
+img2 = ImageTk.PhotoImage(Image.open(path2))
+
+panel2 = tk.Label(tab2, image = img2)
+panel2.grid(column=1, row=0)
 
 window.mainloop()
 
