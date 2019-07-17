2f64b4e3f93b8baca2f1a82d4dad61637976e576
Emily Becher <358274@bsd48.org>
2019-07-17 14:30:07 -0700

    Added Data Entry Tab

--- HandwritingGUI.py
+++ HandwritingGUI.py
@@ -1,12 +1,14 @@
 #Handwriting Classifier GUI
 #7/15/2019
 
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 import subprocess
 import numpy as np
 import PIL
+import os
 
 program = "what"
 
@@ -20,6 +22,14 @@ def run_profiler():
 def run_reader():
     print ("Running Reader")
     #subprocess.call(["python", "reader program"])
+def run():
+    ImagePath = b2.get()
+    Label = b3.get()
+    Width = b4.get()
+    Height = b5.get()
+    Flat = var1.get()
+    os.system("python predict.py --image {} --label-bin {} --width {} --height {} --flatten {}".format(ImagePath, Label, Width, Height, Flat))
+    subprocess.call(["python", "predict.py"])
 
 #Creates Window
 window = tk.Tk()
@@ -30,29 +40,36 @@ window.configure(background = '#778899')
 path1 = "1.jpg"
 path2 = "2.jpg"
 
-#Theme
-#mygrey = "#778899"
-#myred = "#dd0202"
-
-#style = ttk.Style()
-
-#style.theme_create( "theme", parent="alt", settings={
-       # "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0], "background": mygrey } },
-      #  "TNotebook.Tab": {
-  #          "configure": {"padding": [5, 1], "background": mygrey },
-     #       "map":       {"background": [("selected", myred)],}}})
-    #                      "expand": [("selected", [1, 1, 1, 0])] } } } )
-
-
-#style.theme_use("theme")
-
 #Creates Tabs
 tabControl = ttk.Notebook(window)
-tab1 = ttk.Frame(tabControl, height=200)
+tab0 = ttk.Frame(tabControl)
+tabControl.add(tab0, text='Run')
+tab1 = ttk.Frame(tabControl)
 tabControl.add(tab1, text='Programs')
-tabControl.pack(expand=1, fill="both")
-tab2 = ttk.Frame(tabControl, height=200)
+tab2 = ttk.Frame(tabControl)
 tabControl.add(tab2, text="Results")
+tabControl.pack(expand=1, fill="both")
+
+#Tab0
+tk.Label(tab0, text="Enter Information to Run Prediction", font=("Arial Bold", 10)).grid(column=0,row=0, sticky=W)
+tk.Label(tab0, text="Image Path").grid(column=0, row=1, sticky=W)
+tk.Label(tab0, text="Path to Label Binarizar").grid(column=0, row=2, sticky=W)
+tk.Label(tab0, text="Width").grid(column=0, row=3, sticky=W)
+tk.Label(tab0, text="Height").grid(column=0, row=4, sticky=W)
+var1 = IntVar()
+Checkbutton(tab0, text="Flatten Image", variable=var1).grid(column=1, row=5, sticky=W)
+
+b2 = tk.Entry(tab0)
+b2.grid(column=1, row=1)
+b3 = tk.Entry(tab0)
+b3.grid(column=1, row=2)
+b4 = tk.Entry(tab0)
+b4.grid(column=1, row=3)
+b5 = tk.Entry(tab0)
+b5.grid(column=1, row=4)
+
+btnrun=tk.Button(tab0, text='Run Prediction', command=run)
+btnrun.grid(column=0, row=6, sticky=W)
 
 #Tab 1
 lbl = tk.Label(tab1, text="Handwriting Tools", font=("Arial Bold", 20), padx=25, pady=5)
