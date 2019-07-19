ddf0e80ae2465f2c778f749e9a7df6ae67075a90
jason galbraith <galbraithja@beaverton.k12.or.us>
2019-07-19 11:40:22 -0700

    Added Website Files

---
+++ repos/handwriting-classifier/handwriting_classifier/views.py
@@ -0,0 +1,39 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from handwriting_classifier import app
+import subprocess
+
+@app.route('/')
+@app.route('/programs')
+def programs():
+    """Renders the home page."""
+    return render_template(
+        'programs.html',
+        title='Programs',
+        year=datetime.now().year,
+    )
+
+@app.route('/results')
+def results():
+    """Renders the contact page."""
+    return render_template(
+        'results.html',
+        title='Results',
+        year=datetime.now().year,
+        message='Displays results of the calculation.'
+    )
+@app.route('/runpredict')
+def runpredict():
+    """Renders the contact page."""
+    subprocess.call(["python", r"C:\Users\galbraithja\AppData\Local\Programs\Python\Python37-32\predict.py"])
+    return render_template(
+        'runpredict.html',
+        title='Run Predict.py',
+        year=datetime.now().year,
+        message='My attempt at running the neural network algorithm. Yeet!'
+    )
+
---
+++ repos/testoutput/testoutput/testoutput.py
@@ -0,0 +1,13 @@
+import numpy as np
+from PIL import Image
+import PIL
+
+list_im = ['Test1.jpg', 'Test2.jpg']
+imgs    = [ PIL.Image.open(i) for i in list_im ]
+# pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
+min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
+
+# save that beautiful picture
+imgs_comb = PIL.Image.fromarray( imgs_comb)
+imgs_comb.save( 'finalimage.jpg' )    
