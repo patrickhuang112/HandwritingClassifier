051702cc9568e7c1f87e0fd6d1672455f098c78b
Griffin Walraven <griffin.walraven@gmail.com>
2019-07-19 12:49:32 -0700

    .

--- imagecombiner.py
+++ imagecombiner.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 import PIL
 
-list_im = ['photos/1.jpg', 'photos/2.jpg']
+list_im = ['photos/1.jpg', 'photos/3.jpg']
 imgs    = [ PIL.Image.open(i) for i in list_im ]
 # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
 min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
@@ -11,3 +11,6 @@ imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
 # save that beautiful picture
 imgs_comb = PIL.Image.fromarray( imgs_comb)
 imgs_comb.save( 'finalimage.jpg' )    
+img = Image.open('finalimage.jpg')
+img = img.resize((64,64))
+img.save( 'finalimage.jpg' )    
