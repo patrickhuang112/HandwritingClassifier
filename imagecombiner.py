2a0184683d2d0c6b363c8655430d12966d6e45c5
Griffin Walraven <griffin.walraven@gmail.com>
2019-07-22 09:43:32 -0700

    made imagecombiner.py more usable

--- imagecombiner.py
+++ imagecombiner.py
@@ -1,32 +1,22 @@
-<<<<<<< HEAD
 import numpy as np
 from PIL import Image
 import PIL
+import argparse
 
-list_im = ['photos/1.jpg', 'photos/3.jpg']
+ap = argparse.ArgumentParser()
+ap.add_argument("-i1", "--image1", required=True,
+	help="path to input image we are going to classify")
+ap.add_argument("-i2", "--image2", required=True,
+	help="path to second imagel")
+ap.add_argument("-o", "--output_name", required=True,
+        help="name of final image")
+args = vars(ap.parse_args())
+
+list_im = [args["image1"], args["image2"]]
 imgs    = [ PIL.Image.open(i) for i in list_im ]
-# pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
 min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
 imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
 
 # save that beautiful picture
 imgs_comb = PIL.Image.fromarray( imgs_comb)
-imgs_comb.save( 'finalimage1.jpg' )    
-=======
-import numpy as np
-from PIL import Image
-import PIL
-
-list_im = ['photos/1.jpg', 'photos/3.jpg']
-imgs    = [ PIL.Image.open(i) for i in list_im ]
-# pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
-min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
-imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
-
-# save that beautiful picture
-imgs_comb = PIL.Image.fromarray( imgs_comb)
-imgs_comb.save( 'finalimage.jpg' )    
-img = Image.open('finalimage.jpg')
-img = img.resize((64,64))
-img.save( 'finalimage.jpg' )    
->>>>>>> 3d002e980564929c97667330a07cecf18f15ce38
+imgs_comb.save(args["output_name"] + ".jpg")    
