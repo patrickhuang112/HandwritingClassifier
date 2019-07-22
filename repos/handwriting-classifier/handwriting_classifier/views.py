81e6801b617e0a9fcf2684a19cc567dc505bebc3
Griffin Walraven <griffin.walraven@gmail.com>
2019-07-22 10:16:04 -0700

    changed loc stuff on result website tab

--- repos/handwriting-classifier/handwriting_classifier/views.py
+++ repos/handwriting-classifier/handwriting_classifier/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask import render_template
 from handwriting_classifier import app
 import subprocess
+import sys
 
 @app.route('/')
 @app.route('/programs')
@@ -29,7 +30,10 @@ def results():
 @app.route('/runpredict')
 def runpredict():
     """Renders the contact page."""
-    subprocess.call(["python", r"C:\Users\galbraithja\AppData\Local\Programs\Python\Python37-32\predict.py"])
+    if sys.platform == "win32":
+        subprocess.call(["python", r"C:\Users\galbraithja\AppData\Local\Programs\Python\Python37-32\predict.py"])
+    elif sys.platform == "darwin":
+        subprocess.call(["python", r"/Users/griffinwalraven/programming/NWAPW/handwriting classifier/HandwritingClassifier/predict.py"])
     return render_template(
         'runpredict.html',
         title='Run Predict.py',
