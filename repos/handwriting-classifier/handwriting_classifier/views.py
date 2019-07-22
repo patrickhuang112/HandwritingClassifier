ba7f86f186c064dddbe9d065b45f446c332a3e6d
jason galbraith <galbraithja@beaverton.k12.or.us>
2019-07-22 14:37:47 -0700

    Website additions

--- repos/handwriting-classifier/handwriting_classifier/views.py
+++ repos/handwriting-classifier/handwriting_classifier/views.py
@@ -10,45 +10,65 @@ import sys
 import os
 from flask import request
 
-
 @app.route('/')
-@app.route('/programs')
-def programs():
+@app.route('/home')
+def home():
     """Renders the home page."""
     return render_template(
-        'programs.html',
-        title='Programs',
+        'home.html',
+        title='Home',
         year=datetime.now().year,
     )
 
-@app.route('/results')
-def results():
+@app.route('/about')
+def about():
     """Renders the contact page."""
     return render_template(
-        'results.html',
-        title='Results',
+        'about.html',
+        title='About',
         year=datetime.now().year,
         message='Displays results of the calculation.'
     )
-@app.route('/runpredict')
-def runpredict():
+@app.route('/predict')
+def predict():
     """Renders the contact page."""
-    subprocess.call(["python3", r"../../predict.py"])
+    user_id = open("handwriting_classifier/userid.txt").read()
+    if user_id == '1':
+        subprocess.call(["python", r"C:\Users\galbraithja\AppData\Local\Programs\Python\Python37-32\predict.py"])
+    elif user_id == '2':
+        subprocess.call(["python3", r"/Users/griffinwalraven/programming/NWAPW/handwriting classifier/HandwritingClassifier/predict.py"])
+    elif user_id == '3':
+        # put loc of predict.py here
+        pass
+    elif user_id == '4':
+        # put loc of predict.py here
+        pass
+    else:
+        print("Invalid user id")
 
     return render_template(
-        'runpredict.html',
+        'predict.html',
         title='Run Predict.py',
         year=datetime.now().year,
         message='My attempt at running the neural network algorithm. Yeet!'
+    )
+	
+@app.route('/reader')
+def reader():
+    """Renders the contact page."""
+    return render_template(
+        'reader.html',
+        title='Reader',
+        year=datetime.now().year,
+        message='Displays results of the calculation.'
     )
 	
 @app.route('/run', methods=['GET', 'POST'])
 def run():
     if request.method == "POST":
         if request.form['submit'] == 'run':
-            subprocess.call(["python3", "../../HandwritingGUI.py"])
-            
-        return render_template('runpredict.html')
+            subprocess.call(["python", "\..\..\HandwritingClassifier\HandwritingGUI.py"])
+        return render_template('predict.html')
     else:
-        return render_template('results.html')
+        return render_template('reader.html')
 
