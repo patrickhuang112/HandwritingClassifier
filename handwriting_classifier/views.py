e895c299728c4e9cee135bf6aa9e89c5a28fcc28
jason galbraith <galbraithja@beaverton.k12.or.us>
2019-07-23 13:38:06 -0700

    concern

--- handwriting_classifier/views.py
+++ handwriting_classifier/views.py
@@ -42,7 +42,12 @@ def predict():
             Flat = request.form['flat']
             
             os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(ImagePath, Model, Width, Height, str(Flat))) 
-        
+        elif request.form['combinersubmit'] == 'Run':
+            img1 = request.form['firstpath']
+            img2 = request.form['secondpath']
+            name = request.form['resultname']
+            
+            os.system("python imagecombiner.py --image1 {} --image2 {} --output_name {}".format(img1, img2, name)) if win else os.system("python imagecombiner.py --image1 {} --image2 {} --output_name {}".format(img1, img2, name))
       #  if request.form['predictsubmit'] == 'run':
        #     os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(imagepath, model, width, height, str(flat)))
     
