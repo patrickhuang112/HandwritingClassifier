530d2ef75f54b9adc97ed86c1b20c96cf0e6464f
AWOLASAP <griffin.walraven@gmail.com>
2019-07-23 13:45:45 -0700

    .

--- handwriting_classifier/views.py
+++ handwriting_classifier/views.py
@@ -45,7 +45,7 @@ def predict():
             Width = request.form['width']
             Height = request.form['height']
             Flat = request.form['flat']
-            os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(ImagePath, Model, Width, Height, str(Flat))) 
+            os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(ImagePath, Model, Width, Height, str(Flat))) if win else os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(ImagePath, Model, Width, Height, str(Flat)))
 
         elif request.form['combinersubmit'] == 'Run':
             img1 = request.form['firstpath']
@@ -53,17 +53,6 @@ def predict():
             name = request.form['resultname']
             os.system("python imagecombiner.py --image1 {} --image2 {} --output_name {}".format(img1, img2, name)) if win else os.system("python imagecombiner.py --image1 {} --image2 {} --output_name {}".format(img1, img2, name))
 
-      #  if request.form['predictsubmit'] == 'run':
-       #     os.system("python predict.py --image {} --model {} --width {} --height {} --flatten {}".format(imagepath, model, width, height, str(flat)))
-    
-    """if sys.platform.startswith('linix'):
-        subprocess.call(["python3", "predict.py"])
-    elif sys.platform == 'darwin':
-        subprocess.call(["python3", "predict.py"])
-    else:
-        subprocess.call(["python", "predict.py"])"""
-
-    
     return render_template(
         'predict.html',
         title='Predict',
