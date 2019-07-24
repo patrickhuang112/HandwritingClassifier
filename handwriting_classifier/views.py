3c301c67938107f07ccdaeea43be01d2c032c45d
AWOLASAP <griffin.walraven@gmail.com>
2019-07-24 09:57:11 -0700

    rm caching

--- handwriting_classifier/views.py
+++ handwriting_classifier/views.py
@@ -15,6 +15,18 @@ win = False
 if sys.platform == "win32":
     win = True
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -100,4 +112,5 @@ def run():
 @app.route('/game')
 def game():
     return render_template('game.html')
+
 
